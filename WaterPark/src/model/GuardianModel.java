83af2f0fdb1ba1ae36eb548268de3e5b51092090
Assaf <Assaf@Assaf.cs.technion.ac.il>
2016-01-05 19:10:49 +0200

    ergs

--- WaterPark/src/model/GuardianModel.java
+++ WaterPark/src/model/GuardianModel.java
@@ -2,19 +2,40 @@ package model;
 
 import javafx.beans.property.SimpleStringProperty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GuardianModel {
 	public final SimpleStringProperty userName;
 	public SimpleStringProperty password;
-	//TODO array/list of children.
+	public Map<String,ChildModel> children;
 	
 	public GuardianModel(String userName){
 		this.userName = new SimpleStringProperty(userName);
+		this.children = new HashMap<String, ChildModel>();
 	}
 
-	public void setPassword(String password) {
+	public void setPassword(String password){
 		if(password != null)
 			this.password = new SimpleStringProperty(password);
 		//TODO error message: password is set.
+	}
+	
+	public Boolean registerChild (ChildModel child){ //TODO: changed the signature of the function
+		if(children.containsKey(child.getName())){		// TODO: does copying nessesry here??
+			return false;
+		}
+		children.put(child.getName(), child);
+		return true;
+	}
+	
+	public void buyTickets(ChildModel child,RideModel ride, int tickets){
+		if(ride.getIsRgular()){
+			children.get(child.getName()).setRegularTickets(tickets);
+		}
+		else{
+			//TODO: change the extreme ticket number
+		}
 	}
 	 /* TODO
 	  * public void registerChild(Child child);
